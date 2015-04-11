c942e31edd79eadb88452455aa1ba379f01fe136
codefisher <mail@codefisher.org>
2015-04-11 23:06:18 +0200

    there was a crash in the update.rdf if they were on stable, and there should be no update

--- tbutton_maker/views.py
+++ tbutton_maker/views.py
@@ -390,7 +390,7 @@ def update(request):
     if request.GET.get("item_id") == "%ITEM_ID%":
         extension_id = "%s@button.codefisher.org" % hashlib.md5("_".join(sorted(buttons))).hexdigest()
     data = {
-        "applications": list(app_data),
+        "applications": list(app_data) if app_data else None,
         "version": version,
         "update_url": update_url,
         "extension_id": extension_id,
